c11e55d354d3a4974dfd13c410e3011747fe4933
Unknown <bilal-ball@hotmail.fr>
2017-11-10 15:34:50 +0100

    Login, LoginDAO en sha classes
    init push

---
+++ JavaProjectGroep7/src/base/Login.java
@@ -0,0 +1,109 @@
+package base;
+
+import dao.LoginDAO;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="LOGIN")
+
+public class Login extends LoginDAO {
+
+	@Id
+	@Column(name="UserID")
+	private int user_ID;
+	
+	@Column(name="Username")
+	private String username;
+	
+	@Column(name="password")
+	private String password;
+	
+	@Column(name="admin")
+	private boolean Admin;
+	
+	
+	public Login()
+	{
+		
+	}
+	
+	public Login(String username, String password, boolean admin) {
+		super();
+		this.username = username;
+		this.password = password;
+		Admin = admin;
+	}
+
+
+
+	
+	public int getUser_ID() {
+		return user_ID;
+	}
+
+
+
+
+	public Login(String username, String password) {
+		
+		this.username = username;
+		this.password = password;
+	}
+
+	public void setUser_ID(int user_ID) {
+		this.user_ID = user_ID;
+	}
+
+
+
+
+	public String getUsername() {
+		return username;
+	}
+
+
+
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+
+
+	public boolean isAdmin() {
+		return Admin;
+	}
+
+
+
+
+	public void setAdmin(boolean admin) {
+		Admin = admin;
+	}
+
+	@Override
+	public String toString() {
+		return "Login [user_ID=" + user_ID + ", username=" + username + ", password=" + password + ", Admin=" + Admin
+				+ "]";
+	}
+	
+}
---
+++ JavaProjectGroep7/src/base/SHA512.java
@@ -0,0 +1,17 @@
+package base;
+
+import java.security.*;
+import java.math.*;
+
+public class SHA512 {	
+	
+	   public String hashString(String meegegevenString) throws Exception{
+       String s= meegegevenString;
+       MessageDigest m=MessageDigest.getInstance("SHA-512");
+       m.update(s.getBytes(),0,s.length());
+       return new BigInteger(1,m.digest()).toString(16);
+       
+    }
+	
+	
+}
