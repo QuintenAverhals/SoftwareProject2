fb68cbdf3b39fd328a60d37dc4dbbece8852b480
Unknown <seppe.vriens@student.ehb.be>
2017-11-15 13:31:55 +0100

    Certificate af + Survey af (buiten 1 functie)
    Ook een lib file voor connectie met FTP

---
+++ JavaProjectGroep7/src/base/CompoundSurvey.java
@@ -0,0 +1,65 @@
+package base;
+
+import java.io.Serializable;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class CompoundSurvey implements Serializable{
+	
+	private int survey_ID;
+	private int question_ID;
+	
+	
+	
+	public CompoundSurvey() {
+		super();
+	}
+	public CompoundSurvey(int survey_ID, int question_ID) {
+		super();
+		this.survey_ID = survey_ID;
+		this.question_ID = question_ID;
+	}
+	public int getSurvey_ID() {
+		return survey_ID;
+	}
+	public void setSurvey_ID(int survey_ID) {
+		this.survey_ID = survey_ID;
+	}
+	public int getQuestion_ID() {
+		return question_ID;
+	}
+	public void setQuestion_ID(int question_ID) {
+		this.question_ID = question_ID;
+	}
+	
+	@Override
+	public String toString() {
+		return "CompoundSurvey [survey_ID=" + survey_ID + ", question_ID=" + question_ID + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + question_ID;
+		result = prime * result + survey_ID;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CompoundSurvey other = (CompoundSurvey) obj;
+		if (question_ID != other.question_ID)
+			return false;
+		if (survey_ID != other.survey_ID)
+			return false;
+		return true;
+	}
+	
+	
+}
