64481c63e81b8808928e5313e24988ced66919b6
Unknown <chaimae.benhammou@student.ehb.be>
2017-11-27 19:51:34 +0100

    fix errors libraries

--- JavaProjectGroep7/src/dao/TrainingDAO.java
+++ JavaProjectGroep7/src/dao/TrainingDAO.java
@@ -8,8 +8,7 @@ import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-
-import SoftwareProject.Training;
+import base.Status;
 
 public class TrainingDAO {
 
