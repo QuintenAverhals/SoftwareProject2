f81280f005414407b29e8e22f84e29f6f9c30497
Unknown <tom.donche@gmail.com>
2017-12-04 12:20:37 +0100

    usermenu image linkt naar mainmenu

--- JavaProjectGroep7/src/base/userMenuController.java
+++ JavaProjectGroep7/src/base/userMenuController.java
@@ -11,7 +11,18 @@ import javafx.stage.Stage;
 public class userMenuController {
 
 	Button logoutBtn;
+	Button mainMenuBtn;
 	
+	public void mainMenu(ActionEvent event) throws Exception
+	{
+		Parent passwordForgottenParent = FXMLLoader.load(getClass().getResource("mainMenu.fxml"));
+		Scene passwordForgottenScene = new Scene(passwordForgottenParent);
+		
+		Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+		window.setScene(passwordForgottenScene);
+		
+		window.show();
+	}
 	
 	public void logoutBtn(ActionEvent event) throws Exception
 	{
