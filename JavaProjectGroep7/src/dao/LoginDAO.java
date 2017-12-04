b27f7e6bb53ffcc617dd19df5c7478fa2bae0ae7
Unknown <tom.donche@gmail.com>
2017-12-04 15:17:41 +0100

    Revert "Merge remote-tracking branch 'origin/dev_Tom' into dev_Tom"
    This reverts commit 707a0e0d23ced27476b65cad777b253a7849b951.

--- JavaProjectGroep7/src/dao/LoginDAO.java
+++ JavaProjectGroep7/src/dao/LoginDAO.java
@@ -12,13 +12,7 @@ import org.hibernate.cfg.Configuration;
 
 import base.Login;
 import base.SHA512;
-<<<<<<< HEAD
-
 public class LoginDAO {
-	
-=======
-public class LoginDAO {
->>>>>>> 79e4b3fcf11457597d688af4db49c319a74016e2
 
 	public boolean check(Login currentUser) {
 		
