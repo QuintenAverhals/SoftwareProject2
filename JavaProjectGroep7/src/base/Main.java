430d4c477d4eef0f99b8a75f8b8f642c3dbefdd4
Quinten Averhals <quinten.averhals@gmail.com>
2017-12-04 10:58:25 +0100

    Revert "Merge remote-tracking branch 'origin/dev_Tom' into dev_Seppe"
    This reverts commit dfae9172807dc6950883db7399c3bd0eaf0acf68.

--- JavaProjectGroep7/src/base/Main.java
+++ JavaProjectGroep7/src/base/Main.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package base;
 
 
@@ -109,50 +108,3 @@ training.updateStartDate(7, start);
 		
 	}
 }
-=======
-package base;
-
-import java.io.IOException;
-
-import base.buttonsController;
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.StackPane;
-
-
-public class Main extends Application {
-
-    
-	@Override
-	public void start(Stage primaryStage) {
-		primaryStage.setTitle("Main Menu");
-		
-		try {
-			FXMLLoader loader = new FXMLLoader(Main.class.getResource("../GUI/Login.fxml"));
-			loader.setController(new buttonsController());
-			AnchorPane page = (AnchorPane) loader.load();
-			Scene scene = new Scene(page);
-			primaryStage.setScene(scene);
-			primaryStage.show();
-
-			
-		} 
-		catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);	
-		
-		
-	}
-}
->>>>>>> origin/dev_Tom
